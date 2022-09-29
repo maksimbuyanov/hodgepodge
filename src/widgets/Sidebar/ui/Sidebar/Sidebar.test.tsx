580335f6e90c9e32c2608f4409b64e8c30c47d36
maksim.buyanov <maksim.buyanov@gmail.com>
2022-09-29 15:16:31 +0700

    fome fix eslint

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,19 +1,17 @@
-import {fireEvent, render, screen} from "@testing-library/react"
-import {Sidebar} from './Sidebar';
-import {withTranslation} from 'react-i18next';
-import {renderWithTranslations} from '@/shared/lib/renderWithTranslations';
+import { fireEvent, screen } from "@testing-library/react"
+import { Sidebar } from "./Sidebar"
+import { renderWithTranslations } from "@/shared/lib/renderWithTranslations"
 
 describe("Sidebar", () => {
-    test("render", () => {
-        renderWithTranslations(<Sidebar />)
-        expect(screen.getByTestId("sidebar")).toBeInTheDocument()
-    })
-    test("render", () => {
-        renderWithTranslations(<Sidebar />)
-        const toggleButton = screen.getByTestId("sidebar-toggle")
-        expect(screen.getByTestId("sidebar-toggle")).toBeInTheDocument()
-        fireEvent.click(toggleButton)
-        expect(screen.getByTestId("sidebar")).toHaveClass("collapsed")
-    })
-
+  test("render", () => {
+    renderWithTranslations(<Sidebar />)
+    expect(screen.getByTestId("sidebar")).toBeInTheDocument()
+  })
+  test("render", () => {
+    renderWithTranslations(<Sidebar />)
+    const toggleButton = screen.getByTestId("sidebar-toggle")
+    expect(screen.getByTestId("sidebar-toggle")).toBeInTheDocument()
+    fireEvent.click(toggleButton)
+    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed")
+  })
 })
