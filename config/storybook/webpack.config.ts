555c09340ecac813af938333f9be37e97472491e
maksim.buyanov <maksim.buyanov@gmail.com>
2022-09-30 08:25:16 +0700

    fome fix eslint

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -2,9 +2,13 @@ import webpack, { RuleSetRule } from "webpack"
 import { BuildPaths } from "../build/types/config"
 import path from "path"
 import { buildCssLoader } from "../build/loaders/buildCssLoader"
-import {buildSvgLoader} from '../build/loaders/buildSvgLoader';
+import { buildSvgLoader } from "../build/loaders/buildSvgLoader"
 
-export default ({ config }: { config: webpack.Configuration }) => {
+export default ({
+  config,
+}: {
+  config: webpack.Configuration
+}): webpack.Configuration => {
   const paths: BuildPaths = {
     build: "",
     entry: "",
@@ -19,17 +23,21 @@ export default ({ config }: { config: webpack.Configuration }) => {
     mainFiles: ["index"], // Задает для каждой папки главный файл index
   }
 
+  if (config) {
+    if (config.module) {
+      if (config.module.rules) {
+        config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+          if ((rule.test as string).includes("svg")) {
+            return { ...rule, exclude: /\.svg/i }
+          }
+          return rule
+        })
 
-
-  // @ts-expect-error
-  config.module?.rules = config.module?.rules?.map((rule: RuleSetRule) => {
-    if (/svg/.test(rule.test as string)) {
-      return { ...rule, exclude: /\.svg/i }
+        config.module.rules.push(buildSvgLoader())
+        config.module.rules.push(buildCssLoader(true))
+      }
     }
-    return rule
-  })
-  config?.module?.rules?.push(buildSvgLoader())
-  config?.module?.rules?.push(buildCssLoader(true))
+  }
 
   return config
 }
