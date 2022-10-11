03c18d542282f594321ca44765433e96c77909d3
maksim.buyanov <maksim.buyanov@gmail.com>
2022-10-11 10:49:59 +0700

    add fake server

---
+++ json-server/index.js
@@ -0,0 +1,54 @@
+const fs = require("fs")
+const jsonServer = require("json-server")
+const path = require("path")
+
+const server = jsonServer.create()
+
+const router = jsonServer.router(path.resolve(__dirname, "db.json"))
+
+const SERVER_PORT = 8000
+const SERVER_DELAY = 800
+
+const handlers = {
+  login: "/login",
+}
+
+server.use(jsonServer.defaults({ bodyParser: true }))
+server.use(async (req, res, next) => {
+  await new Promise(resolve => {
+    setTimeout(resolve, SERVER_DELAY)
+  })
+  next()
+})
+
+server.use((req, res, next) => {
+  if (!req.headers.authorization && !req.path.includes(handlers.login)) {
+    return res.status(403).json({ message: "AUTH ERROR" })
+  }
+  next()
+})
+
+server.post(handlers.login, (req, res) => {
+  const { username, password } = req.body
+  const db = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "db.json"), "UTF-8")
+  )
+  const { users } = db
+
+  const userFromBd = users.find(
+    user => user.username === username && user.password === password
+  )
+  if (userFromBd) {
+    return res.json(userFromBd)
+  }
+  return res.status(403).json({ message: "USER NOT FOUND" })
+})
+
+server.use(router)
+server.listen(SERVER_PORT, () => {
+  const list = Object.keys(handlers).map(hand => {
+    return `${hand} \n`
+  })
+  console.log(`server is running on port ${SERVER_PORT}`)
+  console.log(`handlers list: \n ${list}`)
+})
